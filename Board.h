a9d7c6ea49a4551fa13b62fd8bb52b6e99ece5a3
Carryme1899 <50023189+Carryme1899@users.noreply.github.com>
2019-05-02 15:59:37 +0800

    A very chi-yee board

--- Board.h
+++ Board.h
@@ -2,12 +2,11 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include "Chess.h"
 
 using namespace std;
 
 typedef vector<vector<uint32_t>> BattleArea;
-typedef vector<BattleArea> BattleHistory;
-typedef vector<string> BattleStatus;
 typedef struct
 {
     uint32_t x;
@@ -18,24 +17,21 @@ class ChessBoard
 {
 public:
     ChessBoard(const string &fileName);
-    const void saveFile(const string &fileName);
-    const void loadFile(const string &fileName);
 
-    const uint32_t getChessID(const Coord &location);
-    const string getChessName(const uint32_t &chessID);
-    const void drawBoard();
+    const uint32_t getChessID(const Coord &location) const;
+    const string getChessName(const uint32_t &chessID) const;
+    void drawBoard(const BattleArea &board) const;
 
-    const bool isCheckmate();
-    const bool isCheck();
+    void undo();
+    void redo();
 
-    void recovery();
-    void unrecovery();
+    const uint16_t playerNow() const;
 
 private:
     // A "2 dim vector" that store chess's unique ID
     // Size = 9 * 10   (Definition in .Cpp)
     BattleArea area;
 
-    // Store battle status
-    BattleHistory history;
+    // Chess list
+    vector<Chess> chess;
 };
