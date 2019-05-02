25a1c9c864ff95a6a3aff2d14062bfef026db997
Carryme1899 <50023189+Carryme1899@users.noreply.github.com>
2019-05-02 17:17:55 +0800

    Chess Obj

--- Chess.h
+++ Chess.h
@@ -5,10 +5,36 @@
 
 using namespace std;
 
+typedef struct
+{
+    uint32_t x;
+    uint32_t y;
+} Coord;
+
 class Chess
 {
 public:
-    Chess(const uint32_t);
+    Chess(const uint32_t &uid, const uint32_t &code);
+
+    const void move();
+    const void capture();
+
+    void promptMovement(Coord *container) const;
+    void promptCapture(Coord *container) const;
+
+    const bool isAlive() const;
 
 private:
+    // A uniqueID
+    // Same kind won't have same ID
+    uint32_t uniqueID;
+
+    // Which kind of chess
+    // Code 99 stand for "null chess"
+    uint32_t kindCode;
+
+    bool alive;
+
+    Coord preCoord;
+    Coord oldCoord;
 };
