45f138b9acbdae30b11cbc1ab7cd5f8573623c8a
Carryme1899 <50023189+Carryme1899@users.noreply.github.com>
2019-05-07 23:35:53 +0900

    redefine the code for null chess add assigment method

--- Chess.h
+++ Chess.h
@@ -1,4 +1,6 @@
 #pragma once
+#define NULL_CHESS -1
+
 #include <cstdint>
 #include <string>
 #include <vector>
@@ -14,27 +16,32 @@ typedef struct
 class Chess
 {
 public:
-    Chess(const uint32_t &uid, const uint32_t &code);
+    Chess();
+    Chess(const int32_t& uid, const int32_t& kind, const Coord& loc);
 
-    const void move();
-    const void capture();
+    void move();
+    void capture();
 
     void promptMovement(Coord *container) const;
     void promptCapture(Coord *container) const;
 
+    const int32_t getID() const;
+    const int32_t getKind() const;
     const bool isAlive() const;
 
+    Chess& operator =(const Chess& temp);
 private:
     // A uniqueID
     // Same kind won't have same ID
-    uint32_t uniqueID;
+    // ID -1 stand for "undefine chess"
+    int32_t uniqueID;
 
     // Which kind of chess
-    // Code 99 stand for "null chess"
-    uint32_t kindCode;
+    // Code -1 stand for "null chess"
+    int32_t kindCode;
 
     bool alive;
 
-    Coord preCoord;
-    Coord oldCoord;
+    Coord currCoord;
+    Coord prevCoord;
 };
