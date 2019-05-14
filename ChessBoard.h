560ed89df87d0134fea97d8485bbf34a897f0470
Carryme1899 <50023189+Carryme1899@users.noreply.github.com>
2019-05-14 14:19:13 +0800

    add "getArea" method

--- ChessBoard.h
+++ ChessBoard.h
@@ -16,9 +16,12 @@ public:
     void setChess(const int32_t &uid, const int32_t &kind, const Coord &loc);
     Chess &getChess(const Coord &loc);
     void moveChess(Chess &c, const Coord &cursorLoc);
-	bool isMovable(const Coord &cursorLoc, const Coord &loc,const int player);
+    bool isMovable(const Coord &cursorLoc, const Coord &loc, const int player);
+
     void setTurn(const int32_t &t);
     const int32_t getTurn() const;
+
+    const BattleArea &getArea() const;
 
     void drawBoard(const BattleArea &board) const;
     void undo();
